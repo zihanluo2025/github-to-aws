24f7805ae8297be024da4b1c3b1880b804ac3560
zihanluo2025 <zihan.luo2025@gmail.com>
2026-02-05 11:59:28 +1030

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootEl = document.getElementById("root");
+
+if (!rootEl) {
+  throw new Error("Root element #root not found. Check index.html for <div id='root'></div>.");
+}
+
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
